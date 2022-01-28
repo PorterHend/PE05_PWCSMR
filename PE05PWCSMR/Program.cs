348aa698ae7414418773aa930b8f4b1bac3b9e6c
PorterHend <pwh6204@g.rit.edu>
2022-01-28 09:59:41 -0500

    Initial commit

---
+++ PE05PWCSMR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PE05PWCSMR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
